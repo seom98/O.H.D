bcc5c7c0d437a9100901a7475bdd733e319059bc
seom98 <seo_m98@naver.com>
2023-01-30 19:06:51 +0900

    오류수정완료

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink, Link } from "react-router-dom";
 import LoginPage from "./LoginPage";
 import SignUpPage from "./SignUpPage";
 import HomePage from "./HomePage";
 
 export default function Router() {
   return (
-      <BrowserRouter>
-          <nav>
-              <NavLink to='/'>Home</NavLink>
-              <Link to='/login'>Login</Link>
-              <Link to='/SignUp'>SignUp</Link>
-          </nav>
+    <BrowserRouter>
+      <nav>
+        <NavLink to='/'>Home</NavLink>
+        <Link to='/login'>Login</Link>
+        <Link to='/SignUp'>SignUp</Link>
+      </nav>
 
       <Routes>
         <Route exact path='/' element={<HomePage />} />
