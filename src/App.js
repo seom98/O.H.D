c7b3aa38192a6c0a568a784496c9abadfb447b46
seom98 <seo_m98@naver.com>
2023-02-01 17:02:30 +0900

    서버 통신 시작

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import Router from "./Router";
+import React, {useState, useEffect} from "react";
+
 
 function App() {
+  const [message, setMessage]=useState([]);
+  useEffect(()=>{
+    fetch("/hello")
+        .then((res)=>{
+          return res.json();
+        })
+        .then((data)=>{
+            setMessage(data);
+        });
+  },[]);
   return (
-    <div>
+    <div className="App">
+
+      <header className="App-header">
+        <ul>
+          {message.map((v,idx)=><li key={`${idx}-${v}`}>{v}</li>)}
+        </ul>
+      </header>
       <Router />
     </div>
   );
