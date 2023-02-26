f4c62de90dd8be599a588b8ec3de9f45a16ddebc
seom98 <seo_m98@naver.com>
2023-02-26 17:57:47 +0900

    선물상자 크기 반응횽

--- src/pages/RoomPage.js
+++ src/pages/RoomPage.js
@@ -27,35 +27,59 @@ export default function RoomPage() {
     return (
         <>
             <h1>{room?.title}의 방</h1>
-            <div style={{display: "flex", justifyContent: "center"}}>
-            <div style={{fontSize: "24px"}}>D-{diffInDays}</div>
-            <ProgressBar
-                value={new Date() - new Date(room?.createdAt)}
-                maxValue={new Date(room?.dday) - new Date(room?.createdAt)}/>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <div style={{ fontSize: "24px" }}>D-{diffInDays}</div>
+                <ProgressBar
+                    value={new Date() - new Date(room?.createdAt)}
+                    maxValue={new Date(room?.dday) - new Date(room?.createdAt)} />
             </div>
-            <div className='container'>
-                <div className='div1'>
-                    {giftList.filter((value, index) => index % 3 === 1).map(({ boxColor, ribbonColor }) => (
-                        <GiftBoxIcon key={boxColor} boxColor={boxColor} ribbonColor={ribbonColor} width="100px" height="100px" />
-                    ))}
+            {diffInDays > 0 ? (
+                <div>
+                    <div className='container'>
+                        <div className='div1'>
+                            {giftList.filter((value, index) => index % 3 === 1).map(({ boxColor, ribbonColor }) => (
+                                <GiftBoxIcon key={boxColor} boxColor={boxColor} ribbonColor={ribbonColor} width="100%" />
+                            ))}
+                        </div>
+                        <div className='div2'>
+                            {giftList.filter((value, index) => index % 3 === 0).map(({ boxColor, ribbonColor }) => (
+                                <GiftBoxIcon key={boxColor} boxColor={boxColor} ribbonColor={ribbonColor} width="100%" />
+                            ))}
+                        </div>
+                        <div className='div3'>
+                            {giftList.filter((value, index) => index % 3 === 2).map(({ boxColor, ribbonColor }) => (
+                                <GiftBoxIcon key={boxColor} boxColor={boxColor} ribbonColor={ribbonColor} width="100%" />
+                            ))}
+                        </div>
+                    </div>
+
+                    <CopyToClipboard text={url1}>
+                        <button className="button1">내방 초대하기</button>
+                    </CopyToClipboard>
+                    <CopyToClipboard text={url2}>
+                        <button className="button1">내방 기억하기</button>
+                    </CopyToClipboard>
                 </div>
-                <div className='div2'>
-                    {giftList.filter((value, index) => index % 3 === 0).map(({ boxColor, ribbonColor }) => (
-                        <GiftBoxIcon key={boxColor} boxColor={boxColor} ribbonColor={ribbonColor} width="100px" height="100px" />
-                    ))}
-                </div>
-                <div className='div3'>
-                    {giftList.filter((value, index) => index % 3 === 2).map(({ boxColor, ribbonColor }) => (
-                        <GiftBoxIcon key={boxColor} boxColor={boxColor} ribbonColor={ribbonColor} width="100px" height="100px" />
-                    ))}
-                </div>
-            </div>
-            <CopyToClipboard text={url1}>
-                <button className="button1">내방 초대하기</button>
-            </CopyToClipboard>
-            <CopyToClipboard text={url2}>
-                <button className="button1">내방 기억하기</button>
-            </CopyToClipboard>
+            ) :
+                <div>
+                    <div className='container'>
+                        <div className='div1'>
+                            {giftList.filter((value, index) => index % 3 === 1).map(({ boxColor, ribbonColor }) => (
+                                <button><GiftBoxIcon key={boxColor} boxColor={boxColor} ribbonColor={ribbonColor} width="100px" height="100px" /></button>
+                            ))}
+                        </div>
+                        <div className='div2'>
+                            {giftList.filter((value, index) => index % 3 === 0).map(({ boxColor, ribbonColor }) => (
+                                <button><GiftBoxIcon key={boxColor} boxColor={boxColor} ribbonColor={ribbonColor} width="100px" height="100px" /></button>
+                            ))}
+                        </div>
+                        <div className='div3'>
+                            {giftList.filter((value, index) => index % 3 === 2).map(({ boxColor, ribbonColor }) => (
+                                <button><GiftBoxIcon key={boxColor} boxColor={boxColor} ribbonColor={ribbonColor} width="100px" height="100px" /></button>
+                            ))}
+                        </div>
+                    </div>
+                </div>}
         </>
     );
 }
